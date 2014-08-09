c6c1ca5549e3ae85a7c198de0a8849c7c58794be
Josh Dingus <josh.dingus@gmail.com>
2014-08-09 17:10:17 -0400

    singlepost func working

--- blog/views.py
+++ blog/views.py
@@ -49,17 +49,7 @@ def add_post_post():
     session.commit()
     return redirect(url_for("posts"))
 
-@app.route("/post/<int:id>")
-def posts(id=1):
-
-    posts = session.query(Post)
-    posts = posts.order_by(Post.datetime.desc())
-    posts = posts[start:end]
-
-    return render_template("posts.html",
-        posts=posts,
-        has_next=has_next,
-        has_prev=has_prev,
-        page=page,
-        total_pages=total_pages
-    )
+@app.route("/singlepost/<int:id>")
+def singlepost(id):
+    posts = session.query(Post).get(id)
+    return render_template("singlepost.html",posts=posts)
